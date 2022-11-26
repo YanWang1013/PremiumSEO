21bbbe3a871d58b8335ea37b37973f1608831373
YanWang1013 <hmwangyan18@gmail.com>
2022-11-26 10:17:25 +0800

    remove config

--- config.py
+++ config.py
@@ -1,14 +1,5 @@
 mysql = {
-    # "user": "coronium",
-    # "password": "Lisa1969",
-    # "host": "167.99.28.221"
-    # "user": "coronium",
-    # "password": "Lisa1969",
-    # "host": "138.68.109.5"
-    # "user": "wangyan19b",
-    # "password": "qazwsxed14",
-    # "host": "orangjuic15.ddnsgeek.com"
-    "user": "aisensechenvas",
-    "password": "Aisense1997!!911",
-    "host": "jmkesxu.quikfindsydney.com.au"
+    "user": "",
+    "password": "",
+    "host": ""
 }
